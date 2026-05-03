885d43c74480ac0f31e37dab0e047aa96cf77f36
6626-rar <1262885902@qq.com>
2026-05-03 21:37:01 +0800

    Delete src directory

--- src/startScreen.js
+++
@@ -1,8 +0,0 @@
-class StartScreen {
-    constructor(onStart) {
-        this.onStart = onStart;
-        this.startScreen = document.getElementById('start-screen');
-    }
-}
-
-export default StartScreen;
