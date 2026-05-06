c3c3093f69fcbfe0ffe55d4c5ce4a012b01826b4
6626-rar <1262885902@qq.com>
2026-05-06 23:36:44 +0800

    Add files via upload

---
+++ src/startScreen.js
@@ -0,0 +1,8 @@
+class StartScreen {
+    constructor(onStart) {
+        this.onStart = onStart;
+        this.startScreen = document.getElementById('start-screen');
+    }
+}
+
+export default StartScreen;
